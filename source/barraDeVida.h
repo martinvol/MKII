3d87d263941da4267a0a99a4a0a7bf3d0d056268
mvbattan <mvbattan@gmail.com>
2015-03-28 17:57:49 -0300

    Agregado el .h de barraDeVida

---
+++ source/barraDeVida.h
@@ -0,0 +1,16 @@
+#ifndef BARRA_H
+#define BARRA_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_image.h>
+#include <string>
+
+class barraDeVida {
+        string path;
+        string nombreDelJugador;
+    public:
+        barraDeVida(unsigned char cantidadDeVida, string path, string nombreDelJugador);
+        unsigned char cantidadDeVida;    
+}
+
+#endif
