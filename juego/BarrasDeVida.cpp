6598a44804c357246d9134a5d3aac50eb0bb4cd5
Maxi <mprystupiuk@gmail.com>
2015-03-31 21:44:55 -0300

    Lastima con 'd' Cansa con 'c' Alivia manteniendo 'a'

--- juego/BarrasDeVida.cpp
+++ juego/BarrasDeVida.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void BarraDeVida::Inicializar(int x_inicial, int x_final, int altoPantalla, SDL_Renderer *rendererParam, bool izquierdaParam){
     this->izquierda = izquierdaParam;
     this->muerto = false;
+    this->terminnoDeCansarse = true;
     this->vidaNumerica = 1000;
     this->staminaNumerica = 100;
 
@@ -23,7 +24,7 @@ void BarraDeVida::Inicializar(int x_inicial, int x_final, int altoPantalla, SDL_
     //Comienza en el 10% de la pantalla
     this->y_ini = int(altoPantalla * 0.1f);
     //Usa 10% del alto.
-    this->y_fin = y_ini+ int(altoPantalla * 0.1f);
+    this->y_fin = y_ini+ int(altoPantalla * 0.07f);
 
     this->vida.x = x_ini;
     this->vida.y = y_ini;
@@ -49,21 +50,38 @@ void BarraDeVida::Inicializar(int x_inicial, int x_final, int altoPantalla, SDL_
 
     this->anchoRectanguloInterior = this->vida.w;
 
+
+    int anchoStamina = int((x_final-x_inicial)/4);
+    int altoStamina = int((y_fin - y_ini)/2);
+    staminaVerde = {x_ini, y_fin+1, anchoStamina, altoStamina};
+    staminaRoja = {x_ini, y_fin+1, 0, altoStamina};
+
+    staminaBorde= {x_ini-1, y_fin, anchoStamina+2, altoStamina+2};
+
+
     //La barra derecha carga el danio al reves
     if (izquierda == false){
         this->danio.x = x_fin;
         this->danio.y = y_ini;
         this->danio.w = 0;
         this->danio.h = (y_fin - y_ini);
+        staminaVerde = {x_fin-anchoStamina, y_fin+1, anchoStamina, altoStamina};
+        staminaRoja = {x_fin, y_fin+1, 0, altoStamina};
+        staminaBorde= {x_fin-anchoStamina-1, y_fin, anchoStamina+2, altoStamina+2};
     }
+    this->anchoRectanguloStaminaInterior = this->staminaVerde.w;
 }
 
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+//-----------------------------------------------------------
 void BarraDeVida::Dibujarse(){
     ActualizarAnchos();
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-    //Dibujo el borde blanco
+    //Dibujo el borde blanco para vida y stamina
     SDL_SetRenderDrawColor( renderer, 255, 255, 255, 200 );
     SDL_RenderDrawRect(renderer, &(this->borde));
+    SDL_RenderDrawRect(renderer, &(this->staminaBorde));
 
     if (muerto){
         SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
@@ -80,8 +98,19 @@ void BarraDeVida::Dibujarse(){
         SDL_SetRenderDrawColor( renderer, 255, 0, 0, 200 );
         SDL_RenderFillRect( renderer, &(this->danio) );
 
+        //Barra de stamina (verde)
+        SDL_SetRenderDrawColor( renderer, 0, 255, 0, 230 );
+        SDL_RenderFillRect( renderer, &(this->staminaVerde) );
+
+        //Barra de stamina (rojo)
+        SDL_SetRenderDrawColor( renderer, 255, 0, 0, 230 );
+        SDL_RenderFillRect( renderer, &(this->staminaRoja) );
+
 }
 
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+//-----------------------------------------------------------
 void BarraDeVida::Lastimar(int porcentaje){
 
    if (vidaNumerica <= 0){
@@ -91,19 +120,27 @@ void BarraDeVida::Lastimar(int porcentaje){
    }else
         vidaNumerica -= porcentaje;
 }
-
-void BarraDeVida::ActualizarAnchos(){
-    int anchoDeVidaEsperado = int((vidaNumerica* anchoRectanguloInterior)/1000);
-    int diferencia = (vida.w -anchoDeVidaEsperado);
-    //asi no permite vidas negativas con anchos de danio
-    //mayores a los de anchoRectanguloInterior.
-    if (vidaNumerica <0){
-        muerto = true;
-        return;
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+void BarraDeVida::Aliviar(int porcentaje){
+    if (staminaNumerica < 100){
+        staminaNumerica += porcentaje;
+        terminnoDeCansarse = true;
     }
+    //Analogo al cansancio.
+    //un alivio de 50 con 90 de stamina --> 150 stamina
+    //con esto fijo el maximo a 100.
+    if (staminaNumerica > 100)
+        staminaNumerica= 100;
+}
 
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+void BarraDeVida::ActualizarAnchoDeVida( int anchoDeVidaEsperado){
+    //ACTUALIZO LA VIDA.
     if (anchoDeVidaEsperado <= vida.w){
-
         if (izquierda == true){
             //desplazo el azul a la derecha
             //y lo achico para no salir del borde
@@ -120,4 +157,81 @@ void BarraDeVida::ActualizarAnchos(){
             vida.w -=2;
         }
     }
+}
+
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+void BarraDeVida::ActualizarAnchoDeStamina(int anchoDeStaminaEsperado){
+//ACTUALIZO STAMINAS
+    if ((anchoDeStaminaEsperado <= staminaVerde.w)&& (!terminnoDeCansarse)){
+        if (izquierda == true){
+            //desplazo el verde a la derecha
+            //y lo achico para no salir del borde
+            staminaVerde.x +=1;
+            staminaVerde.w -=1;
+            // crece el danio
+            staminaRoja.w +=1;
+        }else{
+            //se corre el rojo para izq
+            //y crece
+            staminaRoja.x -=1;
+            staminaRoja.w +=1;
+            // se achica la stamina.
+            staminaVerde.w -=1;
+        }
+    }
+
+    if(anchoDeStaminaEsperado > staminaVerde.w){
+        if (izquierda == true){
+            staminaVerde.x -=1;
+            staminaVerde.w +=1;
+
+            staminaRoja.w -=1;
+        }else{
+            staminaRoja.x +=1;
+            staminaRoja.w -=1;
+
+            staminaVerde.w +=1;
+        }
+    }
+}
+
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+void BarraDeVida::Cansar(int porcentaje){
+    if (staminaNumerica > 0)
+        staminaNumerica -= porcentaje;
+    //ej. queda con 10 de stamina, y un ataque le saca 20
+    //Estamina negativa = cansancio se sale de la barra.
+    //Con esto la fijo siempre en cero
+    if (staminaNumerica < 0)
+        staminaNumerica=0;
+    terminnoDeCansarse = false;
+}
+
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+void BarraDeVida::ActualizarAnchos(){
+    int anchoDeStaminaEsperado = int((staminaNumerica* anchoRectanguloStaminaInterior)/100);
+    int diferenciaStamina = (staminaVerde.w - anchoDeStaminaEsperado);
+
+    //para que se vacie la barra.
+    if (diferenciaStamina <=2){
+        terminnoDeCansarse = true;
+    }
+
+    int anchoDeVidaEsperado = int((vidaNumerica* anchoRectanguloInterior)/1000);
+    //int diferencia = (vida.w -anchoDeVidaEsperado);
+    //asi no permite vidas negativas con anchos de danio
+    //mayores a los de anchoRectanguloInterior.
+    if (vidaNumerica <0){
+        muerto = true;
+        return;
+    }
+
+    ActualizarAnchoDeVida(anchoDeVidaEsperado);
+    ActualizarAnchoDeStamina(anchoDeStaminaEsperado);
 }
--- juego/BarrasDeVida.h
+++ juego/BarrasDeVida.h
@@ -10,26 +10,34 @@ public:
     int vidaNumerica, staminaNumerica;
 private:
     void ActualizarAnchos();
+    void ActualizarAnchoDeVida(int anchoDeVidaEsperado);
+    void ActualizarAnchoDeStamina(int anchoDeStaminaEsperado);
 
     bool muerto;
     bool izquierda;
+    bool terminnoDeCansarse;
     SDL_Renderer *renderer;
 
 
     int x_ini, x_fin, y_ini, y_fin;
     int anchoRectanguloInterior;
+    int anchoRectanguloStaminaInterior;
+
     SDL_Rect borde;
     SDL_Rect vida;
     SDL_Rect danio;
     SDL_Rect vacio;
+    SDL_Rect staminaBorde;
 
-    SDL_Rect staminaVerde, staminaRojo;
-
+    SDL_Rect staminaVerde, staminaRoja;
 
 public:
     void Inicializar(int x_inicial, int x_final, int altoPantalla, SDL_Renderer *rendererParam, bool izquierdaParam);
     void Dibujarse();
     void Lastimar(int porcentaje);
+    void Cansar(int porcentaje);
+    void Aliviar(int porcentaje);
+
 
 };
 
