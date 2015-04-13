8fa4ecb82ddd8f598b93646525c7819ed6f7b92f
Dani <danielap.riesgo@gmail.com>
2015-04-12 23:07:27 -0300

    Mergeando las carpetas que no son juego

---
+++ source/escenario.h
@@ -0,0 +1,13 @@
+#ifndef STAGE_H
+#define STAGE_H
+
+
+class Escenario {
+        //SDL_Texture* capaDelPiso;
+        //Capa piso;
+    public:
+        unsigned float ancho, alto, alturaPiso;
+        Escenario(unsigned float ancho, unsigned float alto, unsigned float alturaPiso);    
+}
+#endif
+
