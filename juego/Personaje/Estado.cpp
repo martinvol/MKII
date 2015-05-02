7a98101a7554220ed718b183ac03fc6ace31875d
Dani <danielap.riesgo@gmail.com>
2015-05-02 20:38:43 -0300

    Corrijo velocidad vertical según el master

--- juego/Personaje/Estado.cpp
+++ juego/Personaje/Estado.cpp
@@ -1,6 +1,6 @@
 #include "Estado.hpp"
 
-#define VELOCIDAD_VERTICAL 75
+#define VELOCIDAD_VERTICAL 75*5
 #define VELOCIDAD_HORIZONTAL 60
 
 using namespace std;
@@ -10,11 +10,11 @@ using namespace std;
  * 
  **********************************************************************/  
 Estado::Estado(string ruta, SDL_Renderer* ren, float alto_personaje, float alto_escenario, float ancho_personaje, float ancho_escenario){
-	this->quieto = new Quieto(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / alto_personaje), 0, 2*alto_personaje);
-	this->saltarvertical = new SaltarVertical(ruta, ren, 0, VELOCIDAD_VERTICAL*(alto_personaje / alto_escenario), 2*alto_personaje);
-	this->saltardiagonal = new SaltarDiagonal(ruta, ren, 3*VELOCIDAD_HORIZONTAL*(ancho_personaje / alto_personaje), VELOCIDAD_VERTICAL*(alto_personaje / alto_escenario), 2*alto_personaje);
-	this->caminar = new Caminar(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / alto_personaje), 0, 2*alto_personaje);
-	this->agacharse = new Agacharse(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / alto_personaje), 0, 2*alto_personaje);
+	this->quieto = new Quieto(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / alto_personaje), 0, 0);
+	this->saltarvertical = new SaltarVertical(ruta, ren, 0, VELOCIDAD_VERTICAL*(alto_personaje / ancho_escenario), 2*alto_personaje);
+	this->saltardiagonal = new SaltarDiagonal(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / alto_personaje), VELOCIDAD_VERTICAL*(alto_personaje / ancho_escenario), 2*alto_personaje);
+	this->caminar = new Caminar(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / alto_personaje), 0, 0);
+	this->agacharse = new Agacharse(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / alto_personaje), 0, 0);
 }
 Estado::~Estado(){
 	delete this->quieto;
