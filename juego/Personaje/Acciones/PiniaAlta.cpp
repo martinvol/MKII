4298abe767dc63cb6177290548d4a4a65dd08ba4
Martín Volpe <martin.volpe@gmail.com>
2015-05-11 17:36:14 -0300

    mejorados el rectángulos del jugador cuando está quieto.

--- juego/Personaje/Acciones/PiniaAlta.cpp
+++ juego/Personaje/Acciones/PiniaAlta.cpp
@@ -10,7 +10,7 @@ using namespace std;
 PiniaAlta::PiniaAlta(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset):Accion(6,ruta,ren,despl_x, despl_y, h_max, hue_init, hue_fin, hue_offset){
 	// (float empieza_alto, float empieza_ancho, float porcentaje_alto, float porcentaje_ancho, bool ataque, bool bloqueo)
 	rectangulos->push_back(new Rectangulo(0, 50, 14, 22, false, false)); // Cabeza
-	rectangulos->push_back(new Rectangulo(0, 73, 30, 30, true, false)); // piña
+	rectangulos->push_back(new Rectangulo(0, 73, 15, 30, true, false)); // piña
 	rectangulos->push_back(new Rectangulo(20, 20, 80, 50, false, false)); // piernas
 
 	this->porcentajeDeDanio = 3;
--- juego/Personaje/Acciones/Quieto.cpp
+++ juego/Personaje/Acciones/Quieto.cpp
@@ -11,7 +11,8 @@ Quieto::Quieto(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, flo
 	cout <<"quieto "<< this->rectangulos->size() << '\n';
 	// (float empieza_alto, float empieza_ancho, float porcentaje_alto, float porcentaje_ancho, bool ataque, bool bloqueo)
 	rectangulos->push_back(new Rectangulo(0, 50, 14, 22, false, false)); // Cabeza
-	rectangulos->push_back(new Rectangulo(20, 0, 30, 100, false, false)); // torso
+	rectangulos->push_back(new Rectangulo(20, 0, 15, 80, false, false)); // torso
+	rectangulos->push_back(new Rectangulo(35, 0, 15, 100, false, false)); // torso
 	rectangulos->push_back(new Rectangulo(49, 20, 50, 70, false, false)); // piernas
 };
 
