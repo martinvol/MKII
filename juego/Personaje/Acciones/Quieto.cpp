2ff4faefe82fe87bdcf35637555a217905e88b0a
mvbattan <mvbattan@gmail.com>
2015-05-11 03:52:07 -0300

    Sacar rectángulo violento D:

--- juego/Personaje/Acciones/Quieto.cpp
+++ juego/Personaje/Acciones/Quieto.cpp
@@ -9,8 +9,6 @@ using namespace std;
 
 Quieto::Quieto(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset):Accion(0,ruta,ren, despl_x, despl_y, h_max, hue_init, hue_fin, hue_offset){
 	cout <<"quieto "<< this->rectangulos->size() << '\n';
-	Rectangulo* todo  = new Rectangulo(0,0,100,100,true,false);
-	rectangulos->push_back(todo);
 	// (float empieza_alto, float empieza_ancho, float porcentaje_alto, float porcentaje_ancho, bool ataque, bool bloqueo)
 	rectangulos->push_back(new Rectangulo(0, 50, 14, 22, false, false)); // Cabeza
 	rectangulos->push_back(new Rectangulo(20, 0, 30, 100, false, false)); // torso
