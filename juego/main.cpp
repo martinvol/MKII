71e8b2969c6fdb64d5b52640b4accf5220491234
Martín Volpe <martin.volpe@gmail.com>
2015-05-13 17:47:27 -0300

    Saqué comentarios pre-historicos.

--- juego/main.cpp
+++ juego/main.cpp
@@ -245,40 +245,14 @@ public:
         }
 }
 
-//----------------------------------------------------------------
-//----------------------------------------------------------------
     void configurar(){
-/*<<<<<<< HEAD
-        cargar_configuracion();
 
-        window = SDL_CreateWindow("Mortal Kombat 3 Ultimate",
-                                   SDL_WINDOWPOS_CENTERED,
-                                   SDL_WINDOWPOS_CENTERED,
-                                   conf->ventana_anchopx, conf->ventana_altopx,
-                                   SDL_WINDOW_MAXIMIZED);
-
-        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-
-        under = loadTexture("resources/background/p_under.png", renderer);
-        cargar_capas();
-
-        //Izquierda
-        
-       //Derecha
-        barraDeVida2.Inicializar(conf->ventana_anchopx/2, conf->ventana_anchopx, conf->ventana_altopx, renderer, false);
-        estado = new Estado((string)(this->conf->sprites_map["personaje1"]), renderer, conf->personaje_alto, conf->escenario_alto, conf->personaje_ancho, conf->escenario_ancho);
-        Personaje* personaje = new Personaje(new CoordenadaLogica(x_logico_personaje,conf->escenario_ypiso),&barraDeVida1,"Subzero",renderer, conf->personaje_alto, conf->personaje_ancho, estado, conf);
-        this->personajeJuego = personaje;
-=======*/
         this->parser = new Parser();
         cargar_configuracion(this->parser);
 		
 		// Para dos personajes:
 		director = new Director(this->escenario, this->ventana, this->conversor, this->personajeJuego, this->personajeJuego2, barraDeVida1, barraDeVida2, FACTOR_SCROLL, this->timer);
-		
-//>>>>>>> remotes/origin/ClonarMaster
-
-        
+		        
     }
 //----------------------------------------------------------------
 //----------------------------------------------------------------
@@ -362,19 +336,8 @@ public:
 		
         configurar();
 
-/*<<<<<<< HEAD
-        estado = new Estado((string)(this->conf->sprites_map["personaje1"]), renderer, conf->personaje_alto, conf->escenario_alto, conf->personaje_ancho, conf->escenario_ancho);
-        this->personajeJuego = new Personaje(new CoordenadaLogica(x_logico_personaje,conf->escenario_ypiso),&barraDeVida1,"Subzero",renderer, conf->personaje_alto, conf->personaje_ancho, estado, conf);
-        cargar_capas();
-        SDL_SetWindowSize(window, conf->ventana_anchopx, conf->ventana_altopx); // Dani se encarga de poner esto en su objeto
-        this->personajeJuego->barraDeVida->Inicializar(0, conf->ventana_anchopx/2, conf->ventana_altopx, renderer, true);
-        barraDeVida2.Inicializar(conf->ventana_anchopx/2, conf->ventana_anchopx, conf->ventana_altopx, renderer, false);
-        this->timer->reset(SDL_GetTicks());
-=======
->>>>>>> remotes/origin/ClonarMaster*/
     };
-//----------------------------------------------------------------
-//----------------------------------------------------------------
+
     void terminar_juego(){
         SDL_JoystickClose(Player1);
         SDL_JoystickClose(Player2);
@@ -383,8 +346,7 @@ public:
         delete this->director; 	// Elimina, conversor, jugadores (personajes y barras de vida), timer, escenario, ventana
         logger->log_debug("Borramos todos los objetos");
     };
-//----------------------------------------------------------------
-//----------------------------------------------------------------
+
     void terminar_sdl(){
         SDL_DestroyRenderer(renderer);
         IMG_Quit();
@@ -392,8 +354,6 @@ public:
     };
 
 
-//----------------------------------------------------------------
-//----------------------------------------------------------------
 void DibujarTodo(){
         //Limpio y dibujo
         
