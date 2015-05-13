236c00549e58917f1e59f98de5a093166efc0a5f
Martín Volpe <martin.volpe@gmail.com>
2015-05-13 05:00:34 -0300

    patada agachado tiene rectangulos y colisiona.

--- juego/Personaje/Acciones/PatadaBajaAgachado.cpp
+++ juego/Personaje/Acciones/PatadaBajaAgachado.cpp
@@ -10,9 +10,10 @@ using namespace std;
 
 PatadaBajaAgachado::PatadaBajaAgachado(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset):Accion(13,ruta,ren, despl_x, despl_y, h_max, hue_init, hue_fin, hue_offset){
 	// (float empieza_alto, float empieza_ancho, float porcentaje_alto, float porcentaje_ancho, bool ataque, bool bloqueo)
-	rectangulos->push_back(new Rectangulo(0, 50, 14, 22, false, false)); // Cabeza
-	rectangulos->push_back(new Rectangulo(20, 0, 30, 100, false, false)); // torso
-	rectangulos->push_back(new Rectangulo(49, 20, 50, 70, false, false)); // piernas
+	rectangulos->push_back(new Rectangulo(0, 30, 14, 22, false, false)); // Cabeza
+	rectangulos->push_back(new Rectangulo(20, 0, 30, 50, false, false)); // torso
+	rectangulos->push_back(new Rectangulo(49, 10, 50, 40, false, false)); // piernas
+	rectangulos->push_back(new Rectangulo(49, 20, 50, 75, true, false)); // piernas
 };
 
 CoordenadaLogica* PatadaBajaAgachado::execute(CoordenadaLogica* coord_personaje){
