d40752c6cfc4062ece49922dc22c1428bbaaf6b0
mvbattan <mvbattan@gmail.com>
2015-05-13 07:13:12 -0300

    Arreglar nombres (clase e includes)

--- juego/Personaje/Acciones/Gancho.hpp
+++ juego/Personaje/Acciones/Gancho.hpp
@@ -1,11 +1,11 @@
-#ifndef TRABA_H_INCLUDED
-#define TRABA_H_INCLUDED
+#ifndef GANCHO_H_INCLUDED
+#define GANCHO_H_INCLUDED
 
 #include "Accion.hpp"
 
-class Traba:public Accion{
+class Gancho:public Accion{
 	public:
-		Traba(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset);
+		Gancho(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset);
 		CoordenadaLogica* execute(CoordenadaLogica* coord_personaje);	
 		bool permiteAccion(accion_posible nuevaAccion);
 		void cambiarModo();
--- juego/Personaje/Acciones/RoundKick.hpp
+++ juego/Personaje/Acciones/RoundKick.hpp
@@ -1,11 +1,11 @@
-#ifndef TRABA_H_INCLUDED
-#define TRABA_H_INCLUDED
+#ifndef ROUNDKICK_H_INCLUDED
+#define ROUNDKICK_H_INCLUDED
 
 #include "Accion.hpp"
 
-class Traba:public Accion{
+class RoundKick:public Accion{
 	public:
-		Traba(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset);
+		RoundKick(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset);
 		CoordenadaLogica* execute(CoordenadaLogica* coord_personaje);	
 		bool permiteAccion(accion_posible nuevaAccion);
 		void cambiarModo();
