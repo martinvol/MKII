6c008a5e44dd40c76db03e113978118e649a5dc5
Martín Volpe <martin.volpe@gmail.com>
2015-05-13 15:09:24 -0300

    patada alta tiene rectangulos, aunque se ejecuta muy rápido

--- juego/Personaje/Acciones/PatadaAlta.cpp
+++ juego/Personaje/Acciones/PatadaAlta.cpp
@@ -9,9 +9,10 @@ using namespace std;
 
 PatadaAlta::PatadaAlta(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset):Accion(11,ruta,ren,despl_x, despl_y, h_max, hue_init, hue_fin, hue_offset){
 	// (float empieza_alto, float empieza_ancho, float porcentaje_alto, float porcentaje_ancho, bool ataque, bool bloqueo)
-	rectangulos->push_back(new Rectangulo(0, 50, 14, 22, false, false)); // Cabeza
-	rectangulos->push_back(new Rectangulo(0, 73, 15, 30, true, false)); // piña
-	rectangulos->push_back(new Rectangulo(20, 20, 80, 50, false, false)); // piernas
+	rectangulos->push_back(new Rectangulo(0, 10, 14, 22, false, false)); // Cabeza
+	rectangulos->push_back(new Rectangulo(15, 65, 15, 38, true, false)); // piña
+	rectangulos->push_back(new Rectangulo(30, 35, 15, 38, true, false)); // piña
+	rectangulos->push_back(new Rectangulo(20, 0, 80, 50, false, false)); // piernas
 
 	this->porcentajeDeDanio = 3;
 };
