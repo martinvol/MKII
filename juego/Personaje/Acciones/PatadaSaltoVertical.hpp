5fe87332d955840d5d3d20a00d75f0f118fc7ef6
mvbattan <mvbattan@gmail.com>
2015-05-13 01:37:05 -0300

    Completar headers de patadas aereas

--- juego/Personaje/Acciones/PatadaSaltoVertical.hpp
+++ juego/Personaje/Acciones/PatadaSaltoVertical.hpp
@@ -1,11 +1,11 @@
-#ifndef PINIAAIRE_H_INCLUDED
-#define PINIAAIRE_H_INCLUDED
+#ifndef PATAVERT_H_INCLUDED
+#define PATAVERT_H_INCLUDED
 
 #include "Accion.hpp"
 
-class PiniaAire:public Accion{
+class PatadaSaltoVertical:public Accion{
 	public:
-		PiniaAire(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset);
+		PatadaSaltoVertical(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset);
 		CoordenadaLogica* execute(CoordenadaLogica* coord_personaje);	
 		bool permiteAccion(accion_posible nuevaAccion);
 		void cambiarModo();
