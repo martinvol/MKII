8640c82c1ff58ba5b928798093b568479049aa0e
mvbattan <mvbattan@gmail.com>
2015-05-15 13:45:47 -0300

    Completar headers

--- juego/Personaje/Acciones/RecibirGolpeAlto.hpp
+++ juego/Personaje/Acciones/RecibirGolpeAlto.hpp
@@ -1,1 +1,15 @@
-vacio
+#ifndef RECIBIRGOLPEALTO_H_INCLUDED
+#define RECIBIRGOLPEALTO_H_INCLUDED
+
+#include "Accion.hpp"
+
+class RecibirGolpeAlto:public Accion{
+	public:
+		RecibirGolpeAlto(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset);
+		CoordenadaLogica* execute(CoordenadaLogica* coord_personaje);	
+		bool permiteAccion(accion_posible nuevaAccion);
+		void cambiarModo();
+};
+
+
+#endif
