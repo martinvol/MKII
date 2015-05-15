fe050d13cb685f1ad303637291efd4d409bdfa87
mvbattan <mvbattan@gmail.com>
2015-05-15 18:45:38 -0300

    Agregar SiendoArrojado al Estado

--- juego/Personaje/Estado.cpp
+++ juego/Personaje/Estado.cpp
@@ -38,6 +38,9 @@ Estado::Estado(string ruta, SDL_Renderer* ren, float alto_personaje, float alto_
 	
 	this->recibirgolpealto = new RecibirGolpeAlto(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / ancho_ventana), 0, 0, hue_init, hue_fin, hue_offset);
 	this->recibirgolpebajo = new RecibirGolpeBajo(ruta, ren, VELOCIDAD_HORIZONTAL*(ancho_personaje / ancho_ventana), 0, 0, hue_init, hue_fin, hue_offset);
+	
+	this->siendoarrojado = new SiendoArrojado(ruta, ren, 3*VELOCIDAD_HORIZONTAL*(ancho_personaje / ancho_ventana), VELOCIDAD_VERTICAL*(alto_personaje / ancho_escenario), 2*alto_personaje, hue_init, hue_fin, hue_offset);
+
 }
 
 Estado::~Estado(){
@@ -64,4 +67,5 @@ Estado::~Estado(){
 	delete this->roundKick;
 	delete this->recibirgolpealto;
 	delete this->recibirgolpebajo;
+	delete this->siendoarrojado;
 }
