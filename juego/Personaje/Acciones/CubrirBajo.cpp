44022f6ce965bcb2e43beb66b1e056bd0ddf5c56
Martín Volpe <martin.volpe@gmail.com>
2015-05-17 05:06:12 -0300

    Cubrirse agachado tiene bien los rectángulos.

--- juego/Personaje/Acciones/CubrirBajo.cpp
+++ juego/Personaje/Acciones/CubrirBajo.cpp
@@ -10,8 +10,9 @@ using namespace std;
 
 CubrirBajo::CubrirBajo(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset):Accion(10,ruta,ren, despl_x, despl_y, h_max, hue_init, hue_fin, hue_offset){
 	// (float empieza_alto, float empieza_ancho, float porcentaje_alto, float porcentaje_ancho, bool ataque, bool bloqueo)
-	rectangulos->push_back(new Rectangulo(0, 50, 14, 22, false, false)); // Cabeza
-	rectangulos->push_back(new Rectangulo(20, 0, 30, 100, false, false)); // torso
+	rectangulos->push_back(new Rectangulo(0, 30, 20, 22, false, false)); // Cabeza
+	rectangulos->push_back(new Rectangulo(0, 55, 30, 30, false, true)); // torso
+	rectangulos->push_back(new Rectangulo(20, 0, 30, 70, false, true)); // torso
 	rectangulos->push_back(new Rectangulo(49, 20, 50, 70, false, false)); // piernas
 };
 
