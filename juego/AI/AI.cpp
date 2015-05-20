c18fc5310be4dc8fd1a9e4873c80db958302e047
mvbattan <mvbattan@gmail.com>
2015-05-20 02:59:03 -0300

    Agregar documentación sobre el método reaccionar()

--- juego/AI/AI.cpp
+++ juego/AI/AI.cpp
@@ -7,5 +7,16 @@ AI::AI(Personaje* personajeAI, Personaje* personajeHumano) {
 }
 
 void AI::reaccionar(){
+// Esta funcion en primera instancia, modificara los flags internos
+// del joystick del personaje.
+
+// Flags disponibles: 
+
+//	Nada, Izquierda, Derecha, Arriba, Abajo;
+//	PiniaAlta, PiniaBaja;
+//	PatadaAlta, PatadaBaja;
+//	ArrojarArma, CubrirAlto;
+	
 	this->personajeAI->Abajo = true;
+	
 }
