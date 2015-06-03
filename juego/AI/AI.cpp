078f4607f07eb0173f83881b924c7db3c8aab0a2
mvbattan <mvbattan@gmail.com>
2015-06-03 18:29:49 -0300

    Agregar probabilidad de reacción

--- juego/AI/AI.cpp
+++ juego/AI/AI.cpp
@@ -1,11 +1,21 @@
 #include "AI.hpp"
 #include "../Coordenadas/CoordenadaLogica.hpp"
 #include <cmath>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+
+#define PROBA_REACCION 0.76
 
 AI::AI(Personaje* personajeAI, Personaje* personajeHumano) {
 	
 	this->personajeAI = personajeAI;
 	this->personajeHumano = personajeHumano;
+	srand (static_cast <unsigned> (time(0)));
+}
+
+float ran01() {
+	return ((float) rand()) / (float) RAND_MAX;
 }
 
 bool left(Personaje* p) {
@@ -35,5 +45,7 @@ void AI::reaccionar(){
 
 // 	Para poder usarlo, setear la constante USAR_AI en true en el main ! 
 	
-	this->personajeAI->CubrirAlto = true;	
+	if (ran01() > PROBA_REACCION) return;
+	
+	this->personajeAI->CubrirAlto = !this->personajeAI->CubrirAlto;	
 }
