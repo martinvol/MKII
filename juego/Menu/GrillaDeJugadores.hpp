cb7fd9973c52d4afab034a099b53d4254bfdf1aa
mvbattan <mvbattan@gmail.com>
2015-06-06 01:59:43 -0300

    Posibles métodos para la Grilla

--- juego/Menu/GrillaDeJugadores.hpp
+++ juego/Menu/GrillaDeJugadores.hpp
@@ -1,4 +1,21 @@
 #ifndef GRILLA_H_INCLUDED
 #define GRILLA_H_INCLUDED
 
+#include <SDL2/SDL.h>
+#include <vector>
+#include <string>
+#include "../Director/Director.hpp"
+
+using namespace std;
+
+class Grilla {
+	private:
+		SDL_Renderer* ren;
+		vector<string> paths;
+	public:
+		Grilla(SDL_Renderer* renderer);
+		void Dibujarse();
+		string obtenerPathJugadorNumero(num_jugador jugador);		
+}
+
 #endif
