b034ecb9c18e3ea864ecf26c6f7a0f12cf1cd433
mvbattan <mvbattan@gmail.com>
2015-06-09 19:07:48 -0300

    Agregar constantes de las carpetas

--- juego/Personaje/Acciones/Desaparecer.cpp
+++ juego/Personaje/Acciones/Desaparecer.cpp
@@ -7,7 +7,7 @@ using namespace std;
  * 
  **********************************************************************/  
 
-Desaparecer::Desaparecer(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset):Accion(0,ruta,ren, despl_x, despl_y, h_max, hue_init, hue_fin, hue_offset){
+Desaparecer::Desaparecer(string ruta, SDL_Renderer* ren, float despl_x, float despl_y, float h_max, int hue_init, int hue_fin, int hue_offset):Accion(43,ruta,ren, despl_x, despl_y, h_max, hue_init, hue_fin, hue_offset){
 	// (float empieza_alto, float empieza_ancho, float porcentaje_alto, float porcentaje_ancho, bool ataque, bool bloqueo)
 	
 	rectangulos->push_back(new Rectangulo(0, 50, 14, 22, false, false)); // Cabeza
